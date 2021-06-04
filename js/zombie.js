6f690185ce5d429f06347574794494dd2555956e
Woodlouse <m.mamontov-2024@kpi.ua>
2021-06-04 20:46:46 +0300

    3 blocks, slider with juxtaposeJS

--- js/zombie.js
+++ js/zombie.js
@@ -1,9 +1,6 @@
 let face = document.querySelector('.preview__face')
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY === 0) {
-        face.classList.remove('zombie')
-    }
     face.classList.add('zombie')
 })
 
